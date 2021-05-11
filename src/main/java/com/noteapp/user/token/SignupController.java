3f0ae56f046d681de98101ef14f8c88c25ee593a
PiraMIID <55114984+PiraMIID@users.noreply.github.com>
2021-05-11 10:49:18 +0200

    mail template and config

--- src/main/java/com/noteapp/user/token/SignupController.java
+++ src/main/java/com/noteapp/user/token/SignupController.java
@@ -1,35 +1,46 @@
 package com.noteapp.user.token;
 
+import com.noteapp.email.MailService;
 import com.noteapp.user.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import springfox.documentation.spring.web.json.Json;
 
+import javax.mail.MessagingException;
+import javax.swing.text.BadLocationException;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 
 @RequestMapping("")
 @RestController
 public class SignupController {
 
-    private TokenService tokenService;
+    private final TokenService tokenService;
+    private final MailService mailService;
 
-    public SignupController(TokenService tokenService) {
+    public SignupController(TokenService tokenService, MailService mailService) {
         this.tokenService = tokenService;
+        this.mailService = mailService;
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Json> save(@RequestBody @Valid SignupRequest signupRequest) {
+    public ResponseEntity<Json> save(@RequestBody @Valid SignupRequest signupRequest) throws MessagingException, IOException, BadLocationException {
         User savedUser = tokenService.save(signupRequest);
         Json message = new Json("{\"message\": \"We are send message on " + signupRequest.getEmail() +" with link. Please confirm to get access to you account. Thanks for join\"}");
+        mailService.sendMail("dszmajduch@gmail.com", signupRequest.getUsername(),"token");
         URI location = ServletUriComponentsBuilder
                 .fromPath("login")
                 .build(savedUser);
         return ResponseEntity.created(location).body(message);
 
+    }
+
+    @GetMapping("/template")
+    @ResponseBody
+    public String bodyEmailMessage() {
+        return "static/mailMessage.html";
     }
 }
